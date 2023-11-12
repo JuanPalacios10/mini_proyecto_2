aa8eb53a82f58906d7719e0436a6885526f2a86d
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2023-11-12 12:50:49 -0500

    Implementacion de la interfaz de actualizar y buscar candidato

--- src/App.java
+++ src/App.java
@@ -70,7 +70,7 @@ public class App extends javax.swing.JFrame {
         );
         panelMenusLayout.setVerticalGroup(
             panelMenusLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 310, Short.MAX_VALUE)
+            .addGap(0, 318, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout panelPrincipalLayout = new javax.swing.GroupLayout(panelPrincipal);
@@ -92,7 +92,7 @@ public class App extends javax.swing.JFrame {
             .addGroup(panelPrincipalLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(tituloPrincipal)
-                .addGap(26, 26, 26)
+                .addGap(18, 18, 18)
                 .addComponent(panelMenus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
             .addComponent(panelSeparador, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -109,9 +109,19 @@ public class App extends javax.swing.JFrame {
         menuCandidatos.add(menuInsertar);
 
         menuActualizar.setText("Actualizar Candidatos");
+        menuActualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuActualizarActionPerformed(evt);
+            }
+        });
         menuCandidatos.add(menuActualizar);
 
         menuBuscar.setText("Buscar Candidatos");
+        menuBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuBuscarActionPerformed(evt);
+            }
+        });
         menuCandidatos.add(menuBuscar);
 
         menuEliminar.setText("Eliminar Candidatos");
@@ -158,6 +168,28 @@ public class App extends javax.swing.JFrame {
         panelMenus.revalidate();
         panelMenus.repaint();
     }//GEN-LAST:event_menuInsertarActionPerformed
+
+    private void menuActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuActualizarActionPerformed
+        PanelMenuActualizar panelMenuActualizar = new PanelMenuActualizar();
+        panelMenuActualizar.setSize(panelMenus.getSize());
+        
+        tituloPrincipal.setText("Actualizar Candidato");
+        panelMenus.removeAll();
+        panelMenus.add(panelMenuActualizar);
+        panelMenus.revalidate();
+        panelMenus.repaint();
+    }//GEN-LAST:event_menuActualizarActionPerformed
+
+    private void menuBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuBuscarActionPerformed
+        PanelMenuBuscar panelMenuBuscar = new PanelMenuBuscar();
+        panelMenuBuscar.setSize(panelMenus.getSize());
+        
+        tituloPrincipal.setText("Buscar Candidato");
+        panelMenus.removeAll();
+        panelMenus.add(panelMenuBuscar);
+        panelMenus.revalidate();
+        panelMenus.repaint();
+    }//GEN-LAST:event_menuBuscarActionPerformed
 
     /**
      * @param args the command line arguments
